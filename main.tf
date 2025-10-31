ce649728cc707166bd73d8d303aeef1ad075bb6c
KIngnafa <olayinkarasheed246@gmail.com>
2025-10-31 17:30:09 -0400

    updating root

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ module "EKS-BASE" {
   EKS_COMPONENTS       = var.EKS_COMPONENTS
   launch_template_name = local.launch_template_name
   private_subnet_ids   = module.VPC-BASE.private_subnet_ids
-
 }
 
 module "VPC-BASE" {
@@ -15,4 +14,5 @@ module "VPC-BASE" {
   public_az         = local.public_by_az
   db_subnet_keys    = local.db_subnet_keys
   availability_zone = data.aws_availability_zones.available
+  cluster_name      = local.cluster_name
 }
