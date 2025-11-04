17e404c85026e513023d16d863a8d5206565acfc
KIngnafa <olayinkarasheed246@gmail.com>
2025-11-03 23:34:50 -0500

    updates

--- main.tf
+++ main.tf
@@ -16,17 +16,18 @@ module "VPC-BASE" {
   availability_zone = data.aws_availability_zones.available
   cluster_name      = local.cluster_name
 }
-module "SECURITY-BASE" {
-  source = "git::ssh://git@github.com/KIngnafa/aws-terraform-modules.git//MODULES/SECURITY-BASE/bastion"
 
-  vpc_id = module.VPC-BASE.vpc_id
-}
+# module "SECURITY-BASE" {
+#   source = "git::ssh://git@github.com/KIngnafa/aws-terraform-modules.git//MODULES/SECURITY-BASE/bastion"
 
-module "EC2-BASE" {
-  source = "git::ssh://git@github.com/KIngnafa/aws-terraform-modules.git//MODULES/EC2-BASE"
+#   vpc_id = module.VPC-BASE.vpc_id
+# }
 
-  EC2_COMPONENTS = var.EC2_COMPONENTS
-  security_group = module.SECURITY-BASE.bastion_sg_id
-  public_az      = local.public_by_az
-  ami            = data.aws_ami.stack_ami
-}
+# module "EC2-BASE" {
+#   source = "git::ssh://git@github.com/KIngnafa/aws-terraform-modules.git//MODULES/EC2-BASE"
+
+#   EC2_COMPONENTS = var.EC2_COMPONENTS
+#   security_group = module.SECURITY-BASE.bastion_sg_id
+#   public_az      = module.VPC-BASE.public_subnets
+#   ami            = data.aws_ami.stack_ami.id
+# }
